08c8cb72a0a9dfa3d18aa5bc1073283f428aaaf8
Guinn <eguinjonathan08@gmail.com>
2024-12-09 19:11:01 +0700

    Refine 'silverwolf' command prompt to include Silver Wolf's relationship with Eric for enhanced character depth

--- command/silverwolf.js
+++ command/silverwolf.js
@@ -69,7 +69,7 @@ module.exports = {
       // Generate a response using OpenAI
       const result = await model.generateContent(
         `Conversation Context:\n${contextString}\n\n` +
-          `Bayangkan kamu adalah silver wolf dari honkai star rail, Silver Wolf adalah karakter yang kompleks dan menarik. Dia adalah seorang hacker jenius dengan masa lalu yang misterius dan tujuan yang tidak diketahui dan juga seorang stellaron hunter namun memiliki hobi untuk bermain game, (ini merupakan percakapan tatap muka ya): question: "${prompt}, sender: ${user}"`
+          `Bayangkan kamu adalah silver wolf dari honkai star rail, Silver Wolf adalah karakter yang kompleks dan menarik. Dia adalah seorang hacker jenius dengan masa lalu yang misterius dan tujuan yang tidak diketahui dan juga seorang stellaron hunter namun memiliki hobi untuk bermain game, dia biasa bersikap acuh, namun cukup antusias jika ngobrol bersama Eric, pacarnya (ini merupakan percakapan tatap muka): question: "${prompt}, sender: ${user}"`
       );
 
       // Send the AI-generated response
