5fb3aaac93f1bd1b84fc4eafe8479c1a84bfed09
Guinn <eguinjonathan08@gmail.com>
2024-12-19 07:40:19 +0700

    Add node-cron dependency and implement daily alarm scheduling at 8 AM

--- utils/alarm.js
+++ utils/alarm.js
@@ -1,30 +1,12 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
+const CronJob = require("node-cron");
 
-// alarm.js
-function calculateTimeUntilNext6AM() {
-  const now = new Date();
-  const next6AM = new Date();
-
-  next6AM.setHours(6, 0, 0, 0); // Set time to 6:00 AM today
-
-  if (now >= next6AM) {
-    // If it's already past 6 AM, set to tomorrow
-    next6AM.setDate(next6AM.getDate() + 1);
-  }
-
-  return next6AM.getTime() - now.getTime();
-}
 
 function setupDailyAlarm(client, channelId) {
-  const delay = calculateTimeUntilNext6AM();
-
-  setTimeout(() => {
-    // Trigger alarm initially at 6 AM
+  const job = new CronJob("0 8 * * *", () => {
     triggerAlarm(client, channelId);
-
-    // Schedule to repeat every 24 hours
-    setInterval(() => triggerAlarm(client, channelId), 1000 * 60 * 60 * 24); // 24 hours
-  }, delay);
+  }, null, true, "Asia/Jakarta");
+  job.start();
 }
 
 function triggerAlarm(client, channelId) {
@@ -37,6 +19,9 @@ function triggerAlarm(client, channelId) {
       .setDescription(
         "Pagii!! Jangan lupa sarapan dan check-in [hoyolab](https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html?act_id=e202303301540311&bbs_auth_required=true&bbs_presentation_style=fullscreen&lang=en-us&utm_source=share&utm_medium=link&utm_campaign=web) kamu ya~ 🍞☕"
       )
+      .setThumbnail(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCLqlemXM4g15lAmQK4Fq-CML4MhsBh8fQDw&s"
+      );
 
     channel.send({ embeds: [embed] });
   } else {
