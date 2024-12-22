23d4036de59e0ace2b23d09ab6a81055a1a37f91
Guinn <eguinjonathan08@gmail.com>
2024-12-22 20:06:56 +0700

    Enhance avatar command to send an embedded message with user's avatar

--- command/avatar.js
+++ command/avatar.js
@@ -1,11 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
-    data: {
-      name: "avatar",
-      description: "Sends the user's avatar",
-    },
-    execute: async (message) => {
-      const user = message.mentions.users.first() || message.author;
-      const avatar = user.displayAvatarURL({ dynamic: true, size: 4096 });
-      await message.channel.send(avatar);
-    },
-  };
+  data: {
+    name: "avatar",
+    description: "Sends the user's avatar",
+  },
+  execute: async (message) => {
+    const user = message.mentions.users.first() || message.author;
+    const avatar = user.displayAvatarURL({ dynamic: true, size: 4096 });
+    
+    const embed = new MessageEmbed()
+    .setTitle(`${user.username}'s Avatar`)
+    .setImage(avatar)
+    .setColor('#00FF00');
+
+    await message.channel.send({ embeds: [embed] });
+  },
+};
