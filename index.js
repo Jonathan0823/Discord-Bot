6738b220ec11f0b6a994ab460b92ccab4fbfa357
Guinn <eguinjonathan08@gmail.com>
2025-02-07 08:20:21 +0700

    Add health check server to monitor bot status

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ const client = new Client({
 });
 const { setupDailyAlarm, triggerAlarm } = require("./utils/alarm");
 const { songs } = require("./utils/songlist");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Health check server running on port ${port}`);
+});
 
 client.on("ready", async () => {
   const targetChannelId = [
