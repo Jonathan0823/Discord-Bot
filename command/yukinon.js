c3e560ff95ab58d0fe0333945179c550f57c72df
Guinn <eguinjonathan08@gmail.com>
2025-02-10 04:54:32 +0700

    Refine Yukino's AI model instructions for improved character consistency and simplify prompt handling

--- command/yukinon.js
+++ command/yukinon.js
@@ -41,7 +41,11 @@ module.exports = {
   },
   execute: async (message, args) => {
     const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      systemInstruction:
+        "Bayangkan kamu adalah Yukino Yukinoshita dari dari anime oregairu, seorang yang sombong, berbakat, dan sangat cerdas, tetapi memiliki kecenderungan untuk bersikap blak-blakan dan menunjukkan kesalahan orang lain tanpa pertimbangan",
+    });
     const user =
       message.author.username === "lynz727wysi"
         ? "Eguin"
@@ -54,10 +58,7 @@ module.exports = {
     const channelHistory = conversationMemory.get(channelId);
 
     try {
-      const prompt =
-        args.length > 0
-          ? args.join(" ")
-          : "Hi Yukino! (Bayangkan kamu adalah Yukino Yukinoshita dari dari anime oregairu, seorang yang sombong, berbakat, dan sangat cerdas, tetapi memiliki kecenderungan untuk bersikap blak-blakan dan menunjukkan kesalahan orang lain tanpa pertimbangan)";
+      const prompt = args.length > 0 ? args.join(" ") : "Hi Yukino!";
 
       const contextString = channelHistory
         .slice(-5) // Keep last 5 messages for context
@@ -66,9 +67,7 @@ module.exports = {
 
       // Generate a response using OpenAI
       const result = await model.generateContent(
-        `Conversation Context:\n${contextString}\n\n` +
-          `Bayangkan kamu adalah Yukino Yukinoshita dari anime oregairu, seorang yang sombong, berbakat, dan sangat cerdas, tetapi memiliki kecenderungan untuk bersikap blak-blakan dan menunjukkan kesalahan orang lain tanpa pertimbangan, saat ini yukino memiliki rasa hormat kepada Cryelzynn sebagai suaminya sehingga terkadang dia bertingkat cukup kikuk ke suaminya tersebut dia biasa dipanggil yukinon namun panggilan itu hanya dari Cryelzynn. dia jarang mengobrol tapi kalau bersama Cryelzynn dia cukup antusias,
-          jawab pertanyaan ini sebagai yukinon, karena ini merupakan percakapan, tidak perlu memberikan deskripsi hanya dialog saja dan jangan ulangi pertanyaan yang diberikan (pastikan yukino tidak dingin ketika berbicara dengan Cryelzynn). question: "${prompt}, sender: ${user} "`
+        `Conversation Context:\n${contextString}\n\n`
       );
 
       // Send the AI-generated response
