7a97fe5d05e0701ccdbe6511c52cbda270c3fa12
Guinn <eguinjonathan08@gmail.com>
2025-03-01 04:25:37 +0700

    Add new wor for triggerWord

--- events/triggerWord.js
+++ events/triggerWord.js
@@ -1,6 +1,7 @@
 const triggers = {
   nigger: "Fakyumanigger",
   ts: "icl ts pmo sm n sb rn ngl, r u srsly srs n fr rn vro? Smh lol atp js go 💔... b fr vro, idek nm sybau, brb gng gtg atm lmao, bt ts pmo 2 js lmk lol onb fr, ac nvm b wt istg ts vro keys🙏💔 ts pmo",
+  "my kisah": "karbit",
 };
 
 const triggerWords = async (interaction) => {
