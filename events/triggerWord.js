f8457227341ae9f314b6b50a91f54d2f9b61920b
Guinn <eguinjonathan08@gmail.com>
2025-03-05 04:11:44 +0700

    Added new trigger word

--- events/triggerWord.js
+++ events/triggerWord.js
@@ -2,6 +2,7 @@ const triggers = {
   nigger: "Fakyumanigger",
   ts: "icl ts pmo sm n sb rn ngl, r u srsly srs n fr rn vro? Smh lol atp js go 💔... b fr vro, idek nm sybau, brb gng gtg atm lmao, bt ts pmo 2 js lmk lol onb fr, ac nvm b wt istg ts vro keys🙏💔 ts pmo",
   "my kisah": "karbit",
+  nahh: "Nah🥀she🥀got🥀you🥀blushin🥀twin🥀awh🥀hell🥀nah🥀twin🥀you🥀gotta🥀lock🥀up🥀twin🥀this🥀ain🥀even🥀you🥀twin🥀on🥀foenem🥀grave🥀bruh🥀euuahhhh🥀",
 };
 
 const triggerWords = async (interaction) => {
