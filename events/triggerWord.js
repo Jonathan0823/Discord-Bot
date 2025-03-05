8047ed56a44962aa04d39df20daeeea0befa5ca5
Guinn <eguinjonathan08@gmail.com>
2025-03-06 06:12:15 +0700

    Remove offensive trigger words from the trigger word management

--- events/triggerWord.js
+++ events/triggerWord.js
@@ -1,11 +1,6 @@
 const { prisma } = require("../lib/prisma");
 
-let triggers = {
-  nigger: "Fakyumanigger",
-  ts: "icl ts pmo sm n sb rn ngl, r u srsly srs n fr rn vro? Smh lol atp js go 💔... b fr vro, idek nm sybau, brb gng gtg atm lmao, bt ts pmo 2 js lmk lol onb fr, ac nvm b wt istg ts vro keys🙏💔 ts pmo",
-  "my kisah": "karbit",
-  nahh: "Nah🥀she🥀got🥀you🥀blushin🥀twin🥀awh🥀hell🥀nah🥀twin🥀you🥀gotta🥀lock🥀up🥀twin🥀this🥀ain🥀even🥀you🥀twin🥀on🥀foenem🥀grave🥀bruh🥀euuahhhh🥀",
-};
+let triggers = {};
 
 const loadTriggerWords = async () => {
   try {
