eb7377304850dec375119b262e5db6705d1786d6
Guinn <eguinjonathan08@gmail.com>
2025-03-07 14:43:02 +0700

    Clear initial song list to load songs dynamically from the database

--- utils/songlist.js
+++ utils/songlist.js
@@ -1,42 +1,6 @@
 const { prisma } = require("../lib/prisma");
 
-let songs = [
-  "Starry Jet",
-  "Ghost",
-  "Stellar Stellar",
-  "Bluerose",
-  "デビュタントボール",
-  "Next Color Planet",
-  "3時12分",
-  "ソワレ",
-  "Awake",
-  "ムーンライト",
-  "みちづれ",
-  "Comet",
-  "Andromeda",
-  "Newton",
-  "流星群",
-  "綺麗事",
-  "Light the Light",
-  "ビビデバ",
-  "Midnight Mission",
-  "Damn Good Day",
-  "Caramel Pain",
-  "DEADPOOL",
-  "繭と心",
-  "ビーナスバグ",
-  "Orbital Period",
-  "もうどうなってもいいや",
-  "ジュビリー",
-  "Template",
-  "レクイエム",
-  "The Last Frontier",
-  "シュガーラッシュ",
-  "Capsule",
-  "Out of Frame",
-  "自分勝手Dazzling",
-  "駆けろ",
-];
+let songs = [];
 
 const loadSong = async () => {
   const data = await prisma.songList.findMany();
