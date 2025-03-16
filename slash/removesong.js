ddff3c33ef8addfe082738177918325a9976ff55
Guinn <eguinjonathan08@gmail.com>
2025-03-16 17:03:09 +0700

    add command to remove song list

---
+++ slash/removesong.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder, MessageFlagsBitField } = require("discord.js");
+const { prisma } = require("../lib/prisma");
+const { updateSongList } = require("../utils/songlist");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("removesong")
+    .setDescription("Removes a song from the song list")
+    .addStringOption((option) =>
+      option
+        .setName("song")
+        .setDescription("The song to remove")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const sender = interaction.user.username;
+
+    if (sender !== "lynz727wysi") {
+      await interaction.reply({
+        content: "You are not authorized to use this command.",
+        flags: MessageFlagsBitField.Flags.Ephemeral
+        ,
+      });
+      return;
+    }
+
+    const song = interaction.options.getString("song");
+
+
+    try {
+      await prisma.songList.delete({
+        where: {
+          songName: song,
+        }
+      })
+    } catch (err) {
+      console.error(err);
+      await interaction.reply({
+        content: "There was an error deleting the song.",
+        flags: MessageFlagsBitField.Flags.Ephemeral,
+      });
+    }
+
+    await updateSongList();
+
+    await interaction.reply({
+      content: `Song ${song} has been removed from the song list.`,
+      flags: MessageFlagsBitField.Flags.Ephemeral,
+    });
+
+
+  }
+}
