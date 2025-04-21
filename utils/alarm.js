8744544d808dbb140cfea782f7e75d3015d8b121
Guinn <eguinjonathan08@gmail.com>
2025-04-22 06:52:05 +0700

    refactor: format alarm.js for better readibilty

--- utils/alarm.js
+++ utils/alarm.js
@@ -27,11 +27,11 @@ const loadAlarmId = async () => {
 
 function setupDailyAlarm(client, type) {
   console.log(
-    "Setting up daily alarm for " + channelId + " with type: " + type
+    "Setting up daily alarm for " + channelId + " with type: " + type,
   );
 
   if (scheduledJobs[type]) {
-    scheduledJobs[type].stop(); // Stop previous job
+    scheduledJobs[type].stop();
     console.log(`Previous cron job for ${type} stopped.`);
   }
 
@@ -44,10 +44,35 @@ function setupDailyAlarm(client, type) {
     },
     {
       timezone: "Asia/Jakarta",
-    }
+    },
   );
 
   console.log(`New cron job for ${type} started.`);
+}
+
+async function firstDayOfMonthAlarm(client, channelIds) {
+  if (!Array.isArray(channelIds)) {
+    console.error("channelId must be an array of IDs.");
+    return;
+  }
+
+  cron.schedule("0 0 1 * *", async () => {
+    const messagePromises = channelIds.map(async (id) => {
+      try {
+        const channel = await client.channels.fetch(id);
+        if (channel) {
+          channel.send(
+            "https://media.tenor.com/1aFbnAIg-n8AAAAM/wake-up-first-day.gif",
+          );
+        }
+      } catch (err) {
+        console.error("Error sending first day of month alarm:", err);
+      }
+    });
+
+    await Promise.all(messagePromises);
+    conole.log("First day of month alarm sent to all channels.");
+  });
 }
 
 async function triggerAlarm(client, channelIds, type) {
@@ -79,7 +104,7 @@ async function triggerAlarm(client, channelIds, type) {
           .setLabel("GI ")
           .setStyle(ButtonStyle.Link)
           .setURL(
-            "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&hyl_auth_required=true&hyl_presentation_style=fullscreen&utm_source=share&utm_medium=link&utm_campaign=web"
+            "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&hyl_auth_required=true&hyl_presentation_style=fullscreen&utm_source=share&utm_medium=link&utm_campaign=web",
           );
 
         const button2 = new ButtonBuilder()
@@ -87,7 +112,7 @@ async function triggerAlarm(client, channelIds, type) {
           .setLabel("HSR")
           .setStyle(ButtonStyle.Link)
           .setURL(
-            "https://act.hoyolab.com/bbs/event/signin/hkrpg/e202303301540311.html?act_id=e202303301540311&bbs_auth_required=true&bbs_presentation_style=fullscreen&lang=en-us&utm_source=share&utm_medium=link&utm_campaign=web"
+            "https://act.hoyolab.com/bbs/event/signin/hkrpg/e202303301540311.html?act_id=e202303301540311&bbs_auth_required=true&bbs_presentation_style=fullscreen&lang=en-us&utm_source=share&utm_medium=link&utm_campaign=web",
           );
 
         const button3 = new ButtonBuilder()
@@ -95,13 +120,13 @@ async function triggerAlarm(client, channelIds, type) {
           .setLabel("ZZZ")
           .setStyle(ButtonStyle.Link)
           .setURL(
-            "https://act.hoyolab.com/bbs/event/signin/zzz/e202406031448091.html?act_id=e202406031448091&bbs_auth_required=true&bbs_presentation_style=fullscreen&lang=en-us&utm_source=share&utm_medium=link&utm_campaign=web"
+            "https://act.hoyolab.com/bbs/event/signin/zzz/e202406031448091.html?act_id=e202406031448091&bbs_auth_required=true&bbs_presentation_style=fullscreen&lang=en-us&utm_source=share&utm_medium=link&utm_campaign=web",
           );
 
         const row = new ActionRowBuilder().addComponents(
           button1,
           button2,
-          button3
+          button3,
         );
 
         const color = getRandomColor();
@@ -110,13 +135,13 @@ async function triggerAlarm(client, channelIds, type) {
           .setColor(color)
           .setTitle("Check In Yuk!")
           .setDescription(
-            "Pagii!! Jangan lupa sarapan dan check-in Hoyolab kamu ya~ 🍞☕"
+            "Pagii!! Jangan lupa sarapan dan check-in Hoyolab kamu ya~ 🍞☕",
           )
           .setThumbnail(
-            "https://play-lh.googleusercontent.com/azVwh1OazZcsq6ocxOzH4mccFgs3IP0-RTxlFsoIumIO28RbmNx2YP7PEsqNAyY0ck0=w240-h480-rw"
+            "https://play-lh.googleusercontent.com/azVwh1OazZcsq6ocxOzH4mccFgs3IP0-RTxlFsoIumIO28RbmNx2YP7PEsqNAyY0ck0=w240-h480-rw",
           )
           .setImage(
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCLqlemXM4g15lAmQK4Fq-CML4MhsBh8fQDw&s"
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCLqlemXM4g15lAmQK4Fq-CML4MhsBh8fQDw&s",
           )
           .setFooter({ text: "Hari ini semangat ya! 💪" });
 
@@ -124,10 +149,10 @@ async function triggerAlarm(client, channelIds, type) {
           .setColor(color)
           .setTitle("Selamat Pagi!")
           .setDescription(
-            "Waktunya grinding Rover!\n Jangan lupa untuk kelarin daily dan bantai-bantai echonya ya~ <:chixiayatta:1336876161803882586>"
+            "Waktunya grinding Rover!\n Jangan lupa untuk kelarin daily dan bantai-bantai echonya ya~ <:chixiayatta:1336876161803882586>",
           )
           .setImage(
-            "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnltdjZtMzVldmF0YnY5emV2bndlZ2hoOHZsb2NwaWpkb3BpNmo1cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/bBXf4upe13UNjpGiFu/giphy.gif"
+            "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnltdjZtMzVldmF0YnY5emV2bndlZ2hoOHZsb2NwaWpkb3BpNmo1cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/bBXf4upe13UNjpGiFu/giphy.gif",
           )
           .setFooter({ text: "Hari ini semangat ya! 💪" });
 
@@ -143,13 +168,13 @@ async function triggerAlarm(client, channelIds, type) {
         }
         success = true; // Mark as success if the operation completes without error
         console.log(
-          `Alarm sent successfully! (retry = ${attempts}, clientId = ${id})`
+          `Alarm sent successfully! (retry = ${attempts}, clientId = ${id})`,
         );
       } catch (error) {
         attempts++;
         console.log(
           `Error sending alarm (attempt ${attempts}/${maxRetries}):`,
-          error
+          error,
         );
 
         if (attempts >= maxRetries) {
